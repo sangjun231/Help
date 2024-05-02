a27461d461fc8a5210b9a917bc757f7404543b7c
jonghoon <jonghoon7431@gmail.com>
2024-05-02 16:13:44 +0900

    엔터키검색,탑버튼수정

--- js/main.js
+++ js/main.js
@@ -7,8 +7,7 @@ const options = {
   },
 };
 
-const topButton = document.querySelector(".top_button");
-
+//리스트 불러오기
 async function fetchMovies() {
   let movies = [];
 
@@ -19,10 +18,10 @@ async function fetchMovies() {
 
     movies.push(...pageResults);
   }
-
   return movies;
 }
 
+//카드 띄우기
 async function setCard(movies) {
   const content = document.querySelector(".content");
   console.log(movies);
@@ -37,7 +36,7 @@ async function setCard(movies) {
       `
      <div class="card">
      <div class="back_part">
-     <p>${title}</p>
+     <p class="movie_title">${title}</p>
      <p>${average}</p>
      </div>
      <img class="poster_img" src="http://image.tmdb.org/t/p/w400/${posterImg}" alt="영화포스터" />
@@ -47,10 +46,60 @@ async function setCard(movies) {
   });
 }
 
+//fetchMovies 이후에 setCard 실행
 fetchMovies().then((movies) => {
   setCard(movies);
 });
 
+//버튼, 엔터키 검색
+const searchButton = document.querySelector(".search_button");
+const searchMovie = document.querySelector(".search_input");
+
+function buttonSearch() {
+  searchButton.addEventListener("click", () => {
+    search();
+  });
+}
+
+searchMovie.addEventListener("keyup", (event) => {
+  if (event.keyCode == 13) {
+    searchButton.click();
+  }
+});
+
+buttonSearch();
+
+//검색
+function search() {
+  let card = document.querySelectorAll(".card");
+  let title = document.querySelectorAll(".movie_title");
+
+  if (searchMovie.value === "") {
+    alert("검색어를 입력해주세요");
+    return;
+  }
+
+  for (let i = 0; i < card.length; i++) {
+    if (title[i].textContent.toLowerCase().includes(searchMovie.value.toLowerCase())) {
+      card[i].style.display = "flex";
+    } else {
+      card[i].style.display = "none";
+    }
+  }
+}
+
+//탑버튼
+const topButton = document.querySelector(".top_button");
+
 topButton.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+window.addEventListener("scroll", (event) => {
+  // event.preventDefault;
+  if (scrollY > 50) {
+    topButton.style.opacity = "100";
+  } else {
+    topButton.style.opacity = "0";
+  }
 });
